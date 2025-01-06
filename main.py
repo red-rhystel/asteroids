ddb32680f4160ac25ac4cbf21bf86ae5ec874888
red-rhystel <redrhystel@protonmail.com>
2025-01-06 00:35:32 -0600

    added circleshape and player modules, drew the player

--- main.py
+++ main.py
@@ -3,21 +3,41 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
+    # initialize pygame modules
     pygame.init()
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # create a screen object to display the game
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # create a player object
+    player = Player(SCREEN_WIDTH / 2,
+                    SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        # fill the screen with the color black
         screen.fill("black")
+        # draw the player, must be after
+        # filling but before the refresh
+        player.draw(screen)
+        # refresh the screen
         pygame.display.flip()
+        
+        # limit the framerate to 60 fps
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
