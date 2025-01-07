38c814a25495cb2fe7245e9b553e6bc8fe69b41e
red-rhystel <redrhystel@protonmail.com>
2025-01-07 17:18:14 -0600

    updated main.py with groups
    Added the groups updatable and drawable to main for organization, player class now uses groups to draw/update

--- main.py
+++ main.py
@@ -12,17 +12,20 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    # create groups to hold our objects
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    # add a class variable to put Player classes in groups
+    Player.containers = (updatable, drawable)
     # create a screen object to display the game
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    # create a clock object to manage game speed
     clock = pygame.time.Clock()
     dt = 0
-
     # create a player object
     player = Player(SCREEN_WIDTH / 2,
                     SCREEN_HEIGHT / 2)
-
+    # main game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -30,10 +33,11 @@ def main():
 
         # fill the screen with the color black
         screen.fill("black")
-        player.update(dt)
-        # draw the player, must be after
-        # filling but before the refresh
-        player.draw(screen)
+        # update/draw all objects in updatable/drawable
+        for thing in updatable:
+            thing.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
         # refresh the screen
         pygame.display.flip()
         
