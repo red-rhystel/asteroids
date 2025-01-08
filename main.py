feebb5810cb0c04cf43386638733fb3a0df5dd57
red-rhystel <redrhystel@protonmail.com>
2025-01-07 21:55:43 -0600

    added Shot class to shots group (whoops)

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def main():
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
-    Shot.containers = (updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
     # create a screen object to display the game
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     # create a clock object to manage game speed
